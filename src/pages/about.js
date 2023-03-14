f9a7555164a64f2e3278047427ae7982af268235
Omodot Etukudo <omodot.etukudo@securedrecords.com>
2023-03-14 11:52:16 +0000

    fix:routes

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import Navbar from '../../components/Navbar'
+
+export default function About() {
+  return (
+    
+    <div>
+        <Navbar/>
+    </div>
+      
+      
+    
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import { Lexend } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '../../components/Navbar'
 import Hero from '../../components/home/Hero'
